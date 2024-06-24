9fbaafd49f95acd6962ec08893a0a852d83b92ce
Rahul Paswan <rahulpaswandoon@gmail.com>
2024-06-25 00:10:54 +0530

    save changes

--- TestAngular/src/app/app.module.ts
+++ TestAngular/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductModule } from './products/product.module';
 import { HashComponent } from './hash/hash.component';
-
 
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import { HashComponent } from './hash/hash.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule, // Add FormsModule to the imports array
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
       { path: 'hash', component: HashComponent },
--- TestAngular/src/app/hash/hash.component.ts
+++ TestAngular/src/app/hash/hash.component.ts
@@ -1,44 +1,60 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { throwError, Subject } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
+
 interface HashResponse {
   hash: string;
 }
+
 @Component({
   selector: 'pm-hash',
   templateUrl: './hash.component.html',
   styleUrls: ['./hash.component.css']
 })
-export class HashComponent implements OnInit {
-  attribution = '<a href="https://www.freepik.com/free-vector/graphic-design-geometric-wallpaper_7088577.htm#query=graphic%20design&position=48&from_view=keyword">Image by pikisuperstar</a> on Freepik'
+export class HashComponent implements OnInit, OnDestroy {
+  attribution = '<a href="https://www.freepik.com/free-vector/graphic-design-geometric-wallpaper_7088577.htm#query=graphic%20design&position=48&from_view=keyword">Image by pikisuperstar</a> on Freepik';
   firstName = 'archit';
   hash = '';
   pageTitle = 'Hash Generator';
 
-  constructor(private http: HttpClient)
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   generateHash() {
-    try {
-      if (this.firstName === '') {
-        throw new Error('Please enter a name');
-      }
-      this.http.get<HashResponse>(`http://localhost:5000/api/Hash/${this.firstname.toLowerCase()}`)
-        .subscribe(res => {
-          if (res.hash === '') {
-            throw new Error('Something went wrong');
-          }
-          // if the hash contains '-', its not a valid hash, so throw an error
-          if (res.hash.includes('-')) {
-            throw new Error('Not a valid hash, The hash should not contain a hyphen. Maybe the response from server should be updated');
-          }
-          this.hash = res.hash;
-        });
+    if (this.firstName === '') {
+      console.error('Please enter a name');
+      return;
     }
-    catch (e) {
-      console.log(e);
-    }
-    
+
+    this.http.get<HashResponse>(`http://localhost:5000/api/Hash/${this.firstName.toLowerCase()}`)
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        catchError(err => {
+          console.error('HTTP Error:', err);
+          return throwError(err);
+        })
+      )
+      .subscribe(res => {
+        if (res.hash === '') {
+          console.error('Empty hash received');
+          return;
+        }
+        // Check for hyphen in the hash
+        if (res.hash.includes('-')) {
+          console.error('Not a valid hash, The hash should not contain a hyphen. Maybe the response from server should be updated');
+          return;
+        }
+        this.hash = res.hash;
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
